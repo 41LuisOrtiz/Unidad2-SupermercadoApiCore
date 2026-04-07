6267b1b5e746dfcbb5e5d6dbd6a5a023d8797b3d
Alejandro Ramos <alejandroenamorado52@gmail.com>
2026-04-06 23:17:18 -0600

    Agregando cambios a features

---
+++ SuperMercadoWebApplication/Features/Categoria/ICategoriaDomainService.cs
@@ -0,0 +1,11 @@
+﻿namespace SuperMercadoWebApplication.Features.Categorias.Interfaces
+{
+    public interface ICategoriaDomainService
+    {
+        // Valida si el nombre de la categoría cumple con los estándares (longitud, caracteres, etc.)
+        bool ValidarNombreCategoria(string nombre);
+
+        // Define si una categoría es considerada "Prioritaria" según su descripción
+        bool EsCategoriaEspecial(string descripcion);
+    }
+}
---
+++ SuperMercadoWebApplication/Features/Cliente/IClienteDomainService.cs
@@ -0,0 +1,8 @@
+﻿namespace SuperMercadoWebApplication.Features.Clientes.Interfaces
+{
+    public interface IClienteDomainService
+    {
+        // Valida el formato del RTN (Registro Tributario Nacional)
+        bool ValidarRTN(string rtn);
+    }
+}
